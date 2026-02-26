316a342d4aa096f01e275f6d77082aee68e1dcc7
benefraimov <60926315+benefraimov@users.noreply.github.com>
2026-02-26 14:21:17 +0200

    Refactor MongoDB connection to use environment variable
    Updated the MongoDB connection URI to use the environment variable MONGO_URI.

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
-// mongodb+srv://zoomtechcollege:53Feq3xPEVVTuPv@zoomprojects.edknv.mongodb.net/?retryWrites=true&w=majority&appName=ZoomProjects
+
 async function connectDB() {
-    const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/testDB';
+    const mongoUri = process.env.MONGO_URI;
     try {
         const response = await mongoose.connect(mongoUri)
         // response.
